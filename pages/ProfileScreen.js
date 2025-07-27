2442cdc13fa269f7bd3091d4cec0db175abb3c35
unknown <Anawut2021@gmail.com>
2025-07-27 16:43:56 +0700

    feat: set up React Navigation with Home Details and Profile screens

---
+++ pages/ProfileScreen.js
@@ -0,0 +1,17 @@
+import { View, Text, Button } from "react-native";
+
+export default function ProfileScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>อนวัช หวังสมบูรณ์ดี ทธด.102</Text>
+      <Text></Text>
+      <Button title="Go to Home"
+        onPress={() => navigation.navigate("Home")}
+      />
+      <Text></Text>
+      <Button title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
